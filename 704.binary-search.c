878a98aa28a897223dc16a5f009958096dcfab63
Anushka Raj <56anushkaa@gmail.com>
2026-07-02 11:53:45 +0530

    Solve Binary Search (LeetCode 704)

---
+++ 704.binary-search.c
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=704 lang=c
+ *
+ * [704] Binary Search
+ */
+
+// @lc code=start
+int search(int* nums, int numsSize, int target) {
+    int low = 0, high = numsSize - 1;
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }       
+    return -1;
+}
+// @lc code=end
+
