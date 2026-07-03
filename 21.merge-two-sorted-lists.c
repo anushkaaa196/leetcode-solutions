9fc891f6c29822000431b5a195394949b2c8edcf
Anushka Raj <56anushkaa@gmail.com>
2026-07-03 09:57:11 +0530

    merge-two-sorted-lists (Leetcode 21)

--- 21.merge-two-sorted-lists.c
+++ 21.merge-two-sorted-lists.c
@@ -13,7 +13,24 @@
  * };
  */
 struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
-    
+    struct ListNode* dummy = (struct ListNode*)malloc(sizeof(struct ListNode));
+    struct ListNode* current = dummy;
+
+    while (list1 != NULL && list2 != NULL) {
+        if (list1->val < list2->val) {
+            current->next = list1;
+            list1 = list1->next;
+        } else {
+            current->next = list2;
+            list2 = list2->next;
+        }
+        current = current->next;
+    }
+
+    // Append any remaining nodes from either list
+    current->next = (list1 != NULL) ? list1 : list2;
+
+    return dummy->next; 
 }
 // @lc code=end
 
