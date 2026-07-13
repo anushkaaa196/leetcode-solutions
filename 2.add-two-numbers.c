7535be88baa17f7206b186125325ce9663327e7c
Anushka Raj <56anushkaa@gmail.com>
2026-07-13 23:45:11 +0530

    add-two-numbers(Leetcode 2)

---
+++ 2.add-two-numbers.c
@@ -0,0 +1,43 @@
+ /*
+ * @lc app=leetcode id=2 lang=c
+ *
+ * [2] Add Two Numbers
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+    
+    struct ListNode* dummy = (struct ListNode*)malloc(sizeof(struct ListNode));
+    struct ListNode* current = dummy;
+    int carry = 0;
+
+    while (l1 != NULL || l2 != NULL || carry != 0) {
+        int sum = carry;
+        if (l1 != NULL) {
+            sum += l1->val;
+            l1 = l1->next;
+        }
+        if (l2 != NULL) {
+            sum += l2->val;
+            l2 = l2->next;
+        }
+        carry = sum / 10;
+        current->next = (struct ListNode*)malloc(sizeof(struct ListNode));
+        current->next->val = sum % 10;
+        current->next->next = NULL;
+        current = current->next;
+    }
+
+    struct ListNode* result = dummy->next;
+    free(dummy);
+    return result;
+}
+// @lc code=end
+
